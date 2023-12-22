6984561214dbc76cddd19296e38e20307dea6745
hannah888 <pauline.s1502@gmail.com>
2023-12-22 18:32:03 +0100

    setup auth

---
+++ app.js
@@ -0,0 +1,34 @@
+// LOGIN FORM //
+
+function stringToHash(string) {
+    let hash = 0;
+    if (string.length == 0) return hash;
+    for (i = 0; i < string.length; i++) {
+        char = string.charCodeAt(i);
+        hash = ((hash << 5) - hash) + char;
+        hash = hash & hash;
+    }
+    return hash;
+}
+ 
+function passwordIsCorrect(password) {
+    hashedPassword = stringToHash(password);
+    return hashedPassword == '1217426811';
+}
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "MasterJB" && passwordIsCorrect(password)) {
+        alert("Bienvenue, Maître JB.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
